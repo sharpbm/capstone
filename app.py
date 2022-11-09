b03fd83205f952815a99a33aa1ddd81e74503d18
EC2 Default User <ec2-user@ip-172-31-11-198.ec2.internal>
2022-11-09 06:47:46 +0000

    fix

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/<name>')
 def hello_name(name):
-    message='Hello %s!' % name
-    return "<h1 style='text-align: center;'>{message}</h1>"
+    return 'Hello %s!' % name
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
