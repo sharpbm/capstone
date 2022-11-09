aed6b8a1c71bb9fa328b364a43028d086dd77c02
EC2 Default User <ec2-user@ip-172-31-11-198.ec2.internal>
2022-11-09 04:08:59 +0000

    fix

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_name(name):
     return 'Hello %s!' % name
    
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
