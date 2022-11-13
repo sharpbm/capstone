cd4d01595cc08edb60c6f3c6bb6ede5ba039c8d7
EC2 Default User <ec2-user@ip-172-31-11-198.ec2.internal>
2022-11-13 23:40:07 +0000

    correct lint failure

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/<name>')
 def hello_name(name):
-    return 'Hello %s! % name
+    return 'Hello %s!' % name
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
